7ff1a0838f64c1d731127139b4c20367e7be6581
Gabrielhzt <hazout26@gmail.com>
2024-01-22 15:44:45 +0100

    Initial commit

---
+++ src/components/Data/postdata.js
@@ -0,0 +1,29 @@
+const postdata = [
+    {
+        name: 'gabriel',
+        time: '4h',
+        text: 'je suis en train de coder ! Ne me parler pas ',
+        high: 103,
+        comment: 43
+    }, {
+        name: 'gabriel',
+        time: '4h',
+        text: 'je suis en train de coder ! Ne me parler pas ',
+        high: 103,
+        comment: 43
+    }, {
+        name: 'gabriel',
+        time: '4h',
+        text: 'je suis en train de coder ! Ne me parler pas ',
+        high: 103,
+        comment: 43
+    }, {
+        name: 'gabriel',
+        time: '4h',
+        text: 'je suis en train de coder ! Ne me parler pas ',
+        high: 103,
+        comment: 43
+    }
+]
+
+export default postdata;
